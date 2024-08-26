81f701b3b97ca56d33e43c1a376835c915e19c18
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-08-26 12:54:00 -0500

    improved styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,35 @@
 
 export default function App() {
     return (
-      <div>
+      <div className="flex flex-col h-screen">
         <header className="bg-white shadow-md py-2">
             <h1 className="text-2xl max-w-screen-xl mx-auto">Medimo Deadlines</h1>
         </header>
-        <main>
-            here goes main
-        </main>
+        <div className="mt-2 flex-grow">
+            <main className="max-w-7xl mx-auto">
+                <article>
+                    <header>
+                        <h2>Deadline Vandaag</h2>
+                    </header>
+                    <section>
+                        <form>
+                            <label htmlFor="deadline_today_time">
+                                Uiterste tijd van voorschrijven/bestellen:
+                            </label>
+                            <input type="time" id="deadline_today_time" />
+                        </form>
+                    </section>
+                    <footer>
+                        <button>
+                            Bereken deadline
+                        </button>
+                    </footer>
+                </article>
+            </main>
+        </div>
+        <footer className="py-2 text-center">
+            &copy; Hook88 2024
+        </footer>
       </div>
     )
   }
